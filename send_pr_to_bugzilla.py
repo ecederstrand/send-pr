8b2d885f35816596258e3b84af874cf358366331
Erik Cederstrand <erik@cederstrand.dk>
2012-10-28 21:29:35 +0100

    pylint: Fix whitespace indentation. Probably fix gzip compressed streams (untested). Add constructor to init self.sessioncookie

--- send_pr_to_bugzilla.py
+++ send_pr_to_bugzilla.py
@@ -33,39 +33,39 @@ body = msg.get_payload()
 
 # Parse send-pr email
 for line in body.splitlines(True):
-	if item_key is None and not line.startswith('>'): continue # Empty lines in top of email
-	if line.startswith('>'):
-		# Parse ">SETTING : VALUE" line
-		item_key, sep, item_value = [s.strip() for s in line[1:].partition(':')]
-		assert item_key in valid_send_pr_keys
-		assert item_key not in items
-		items[item_key] = item_value
-		continue
-	if line.startswith('--- ') and line.rstrip().endswith(' begins here ---'):
-		# Start parsing an attachment
-		assert is_attachment == False
-		assert attachment_filename is None
-		assert attachment_content == ''
-		is_attachment = True
-		attachment_filename = line.replace('--- ', '').replace(' begins here ---', '').strip()
-		assert len(attachment_filename) > 0
-		assert attachment_filename not in attachments
-		attachments[attachment_filename] = ''
-		continue
-	if line.startswith('--- ') and line.rstrip().endswith(' ends here ---'):
-		# Attachment ends
-		assert attachment_filename == line.replace('--- ', '').replace(' ends here ---', '').strip()
-		is_attachment = False
-		attachment_filename = None
-		attachment_content = ''
-		continue
-	if is_attachment:
-		#Read attachment contents
-		assert attachment_filename is not None
-		attachments[attachment_filename] += line
-		continue
-	# Multi-line setting values
-	items[item_key] += line.rstrip()
+    if item_key is None and not line.startswith('>'): continue # Empty lines in top of email
+    if line.startswith('>'):
+        # Parse ">SETTING : VALUE" line
+        item_key, sep, item_value = [s.strip() for s in line[1:].partition(':')]
+        assert item_key in valid_send_pr_keys
+        assert item_key not in items
+        items[item_key] = item_value
+        continue
+    if line.startswith('--- ') and line.rstrip().endswith(' begins here ---'):
+        # Start parsing an attachment
+        assert is_attachment == False
+        assert attachment_filename is None
+        assert attachment_content == ''
+        is_attachment = True
+        attachment_filename = line.replace('--- ', '').replace(' begins here ---', '').strip()
+        assert len(attachment_filename) > 0
+        assert attachment_filename not in attachments
+        attachments[attachment_filename] = ''
+        continue
+    if line.startswith('--- ') and line.rstrip().endswith(' ends here ---'):
+        # Attachment ends
+        assert attachment_filename == line.replace('--- ', '').replace(' ends here ---', '').strip()
+        is_attachment = False
+        attachment_filename = None
+        attachment_content = ''
+        continue
+    if is_attachment:
+        #Read attachment contents
+        assert attachment_filename is not None
+        attachments[attachment_filename] += line
+        continue
+    # Multi-line setting values
+    items[item_key] += line.rstrip()
 
 print msg
 
@@ -75,21 +75,25 @@ BZ_PASS = 'anonymous'
 
 import xmlrpclib
 import gzip
+from StringIO import StringIO
 import Cookie
 
 class SessionTransport(xmlrpclib.Transport):
+    def __init__(self, *args, **kwargs):
+        self.sessioncookie = Cookie.SimpleCookie()
+        super(SessionTransport, self).__init__(*args, **kwargs)
+
     def parse_response(self, response):
         # read response data from httpresponse, and parse it
 
         # Check for new http response object, else it is a file object
         if hasattr(response,'getheader'):
             if response.getheader("Content-Encoding", "") == "gzip":
-                stream = GzipDecodedResponse(response)
+                stream = gzip.GzipFile('', 'r', 0, StringIO(response))
             else:
                 stream = response
             cookie = response.getheader('Set-Cookie')
             if cookie:
-                self.sessioncookie = Cookie.SimpleCookie()
                 self.sessioncookie.load(cookie)
         else:
             stream = response
@@ -116,9 +120,9 @@ class SessionTransport(xmlrpclib.Transport):
             connection.putheader("Accept-Encoding", "gzip")
         else:
             connection.putrequest("POST", handler)
-        if hasattr(self, 'sessioncookie'):
-            cookie = self.sessioncookie.output(header='', sep=';')
-            connection.putheader('Cookie', cookie)
+        if len(self.sessioncookie):
+            cookiestr = self.sessioncookie.output(header='', sep=';')
+            connection.putheader('Cookie', cookiestr)
 
 
 bz = xmlrpclib.ServerProxy(XMLAPI_URL, transport=SessionTransport())
@@ -127,40 +131,40 @@ user_id = login.get('id')
 if user_id: print 'login success:', login
 
 bug = bz.Bug.create(dict(
-	product = 'TestProduct',
-	component = 'TestComponent',
-	summary = items['Synopsis'],
-	version = '9.0', #items['Release'], 
-	description = items['Description'] + '\n\nReported by: ' + items['Originator'] + 
-		' (' +  (msg['Reply-To'] or msg['From']) + ')', 
-	op_sys = 'FreeBSD', #items['Environment'],
-	platform = 'All', #items['Release'].strip().split(' ')[-1],
-	priority = items['Priority'].capitalize(),
-	severity = items['Severity'],
-	#alias=...,
-	#assigned_to =...,
-	#cc = [user.strip() for user in msg['Cc']],
-	comment_is_private = True if items['Confidential'] != 'no' else False,
-	#groups=...,
-	#qa_contact=...,
-	#status=...,
-	#resolution=...,
-	#target_milestone=...,)
+    product = 'TestProduct',
+    component = 'TestComponent',
+    summary = items['Synopsis'],
+    version = '9.0', #items['Release'], 
+    description = items['Description'] + '\n\nReported by: ' + items['Originator'] + 
+        ' (' +  (msg['Reply-To'] or msg['From']) + ')', 
+    op_sys = 'FreeBSD', #items['Environment'],
+    platform = 'All', #items['Release'].strip().split(' ')[-1],
+    priority = items['Priority'].capitalize(),
+    severity = items['Severity'],
+    #alias=...,
+    #assigned_to =...,
+    #cc = [user.strip() for user in msg['Cc']],
+    comment_is_private = True if items['Confidential'] != 'no' else False,
+    #groups=...,
+    #qa_contact=...,
+    #status=...,
+    #resolution=...,
+    #target_milestone=...,)
 ))
 bug_id = bug.get('id')
 if bug_id: print 'bug create success', bug
 
 for key, val in attachments.iteritems():
-	print 'add attachment', key, bz.Bug.add_attachment(dict(
-		ids = [int(bug_id)],
-		data = val,
-		file_name = key,
-		summary = 'Added by send-pr',
-		content_type = 'text/plain',
-		#comment = ...,
-		is_patch = True,
-		is_private = True if items['Confidential'] != 'no' else False,
-	))
+    print 'add attachment', key, bz.Bug.add_attachment(dict(
+        ids = [int(bug_id)],
+        data = val,
+        file_name = key,
+        summary = 'Added by send-pr',
+        content_type = 'text/plain',
+        #comment = ...,
+        is_patch = True,
+        is_private = True if items['Confidential'] != 'no' else False,
+    ))
 
 #items['Category']
 #items['Class']
